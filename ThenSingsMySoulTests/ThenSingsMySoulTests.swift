b7b66e6382024294ceda1599e74b13fa0388d5eb
Sasha <sasha@sashas-imac.local>
2017-02-14 19:12:04 -0800

    Initial Commit

---
+++ ThenSingsMySoulTests/ThenSingsMySoulTests.swift
@@ -0,0 +1,36 @@
+//
+//  ThenSingsMySoulTests.swift
+//  ThenSingsMySoulTests
+//
+//  Created by Sasha on 2/14/17.
+//  Copyright © 2017 Novaci. All rights reserved.
+//
+
+import XCTest
+@testable import ThenSingsMySoul
+
+class ThenSingsMySoulTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
