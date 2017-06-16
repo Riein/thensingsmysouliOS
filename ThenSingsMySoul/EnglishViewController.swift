c3e3ed8ad88adde43ed6776d24ad6b93f4d0e258
Sasha <sasha@sashas-imac.local>
2017-06-16 14:39:56 -0700

    Adds a songs view to display songs.

--- ThenSingsMySoul/EnglishViewController.swift
+++ ThenSingsMySoul/EnglishViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class EnglishViewController: UITableViewController {
 
+    var song: [String: AnyObject]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
