ac0b11579115607c790c1f27ea64aff33659cc87
Sasha Demyanik <Sasha@sashas-imac.local>
2017-06-18 17:20:18 -0700

    Adds SongViewController file and connects UI elements.

---
+++ ThenSingsMySoul/SongViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SongViewController.swift
+//  ThenSingsMySoul
+//
+//  Created by Sasha Demyanik on 6/18/17.
+//  Copyright © 2017 Novaci. All rights reserved.
+//
+
+import UIKit
+
+class SongViewController : UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var songTextLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
