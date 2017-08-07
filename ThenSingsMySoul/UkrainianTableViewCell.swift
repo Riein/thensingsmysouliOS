98dd9424e7c8b84704ab2f73db8e6e5e8dba6dd1
Sasha Demyanik <ttsvan@msn.com>
2017-08-06 20:45:17 -0700

    Adds UkrainianTableViewCell.

---
+++ ThenSingsMySoul/UkrainianTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  UkrainianTableViewCell.swift
+//  ThenSingsMySoul
+//
+//  Created by Sasha Demyanik on 8/6/17.
+//  Copyright © 2017 Novaci. All rights reserved.
+//
+
+import UIKit
+
+class UkrainianTableViewCell: UITableViewCell {
+
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ThenSingsMySoul/UkrainianViewController.swift
+++ ThenSingsMySoul/UkrainianViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class UkrainianViewController: UITableViewController {
 
+    //MARK: Properties
+    var songs = [Song]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadUkrainianSongs()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: Private Methods
+    
+    private func loadUkrainianSongs() {
+        guard let song1 = Song(title: "Test", text: "Going to be a longer one") else {
+            fatalError("Unable to instantiate song")
+        }
+        guard let song2 = Song(title: "What", text: "Guess my name yo") else {
+            fatalError("Unable to instantiate song")
+        }
+        
+        guard let song3 = Song(title: "Who", text: "Guess my name yo bandito") else {
+            fatalError("Unable to instantiate song")
+        }
+        
+        songs += [song1, song2, song3]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Table view cells are reused and should be dequeued using a cell identifier
+        let cellIdentifier = "UkrainianTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? UkrainianTableViewCell else {
+            fatalError("The dequeued cell is not an instance of UkrainianTableViewCell")
+        }
+        
+        // Fetches the appropriate song for the data source layout
+        let song = songs[indexPath.row]
+        
+        //configure the cell...
+        cell.songName.text = song.text
+        
+        return cell
+    }
 
 }
 
